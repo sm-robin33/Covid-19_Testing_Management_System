7d0371006d99639c809af30a3a9f79dca0536b3a
robin <smrobin173@gmail.com>
2023-08-30 00:15:33 +0600

    final commit

---
+++ covidtmsdb.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2021 at 03:05 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `covidtmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(11) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `AdminuserName` varchar(20) NOT NULL,
+  `MobileNumber` int(10) NOT NULL,
+  `Email` varchar(120) NOT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `AdminuserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(2, 'Admin', 'admin', 1234567890, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2021-04-19 18:30:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpatients`
+--
+
+CREATE TABLE `tblpatients` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(12) DEFAULT NULL,
+  `DateOfBirth` date DEFAULT NULL,
+  `GovtIssuedId` varchar(150) DEFAULT NULL,
+  `GovtIssuedIdNo` varchar(150) DEFAULT NULL,
+  `FullAddress` varchar(255) DEFAULT NULL,
+  `State` varchar(200) DEFAULT NULL,
+  `RegistrationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblpatients`
+--
+
+INSERT INTO `tblpatients` (`id`, `FullName`, `MobileNumber`, `DateOfBirth`, `GovtIssuedId`, `GovtIssuedIdNo`, `FullAddress`, `State`, `RegistrationDate`) VALUES
+(1, 'Anuj kumar', 1234567890, '1999-02-01', 'Driving License', '342545445345', 'A83748 New Delhi India', 'Delhi', '2021-04-27 17:31:22'),
+(2, 'Sarita', 6547893210, '1990-05-09', 'Pancard', 'HHHGHGH8888jh', 'H 826273 Noida', 'Uttar Pradesh', '2021-04-27 18:04:57'),
+(4, 'Garima Singh', 4598520125, '2005-01-08', 'Pancard', 'DDDKJKJ454545H', 'A-1234 Patna', 'Bihar', '2021-05-08 05:49:44'),
+(5, 'Amit Singh', 2536987410, '2007-06-01', 'PANCARD', 'HJGGHG76767HGGH', 'H 37334 New Delhi', 'Delhi', '2021-05-08 09:25:50'),
+(6, 'Rahul Yadav', 1234567899, '2003-06-05', 'Driving License', '5435345', 'ABC 123 XYZ Street Noida', 'Uttar Pradesh', '2021-05-08 09:29:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblphlebotomist`
+--
+
+CREATE TABLE `tblphlebotomist` (
+  `id` int(11) NOT NULL,
+  `EmpID` varchar(100) DEFAULT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(12) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblphlebotomist`
+--
+
+INSERT INTO `tblphlebotomist` (`id`, `EmpID`, `FullName`, `MobileNumber`, `RegDate`) VALUES
+(3, '12587493', 'Amit Singh', 9876543212, '2021-05-03 04:51:44'),
+(4, '105202365', 'Rahul', 8529631470, '2021-05-03 04:52:06'),
+(5, '10802364', 'Sanjeev Tomar', 1234567890, '2021-05-08 09:34:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblreporttracking`
+--
+
+CREATE TABLE `tblreporttracking` (
+  `id` int(11) NOT NULL,
+  `OrderNumber` bigint(40) DEFAULT NULL,
+  `Remark` varchar(255) DEFAULT NULL,
+  `Status` int DEFAULT NULL,
+  `PostingTime` timestamp NULL DEFAULT current_timestamp(),
+  `RemarkBy` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblreporttracking`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbltestrecord`
+--
+
+CREATE TABLE `tbltestrecord` (
+  `id` int(11) NOT NULL,
+  `OrderNumber` bigint(14) DEFAULT NULL,
+  `PatientMobileNumber` bigint(14) DEFAULT NULL,
+  `TestType` varchar(100) DEFAULT NULL,
+  `TestTimeSlot` varchar(120) DEFAULT NULL,
+  `ReportStatus` int  DEFAULT NULL,
+  `FinalReport` varchar(150) DEFAULT NULL,
+  `ReportUploadTime` varchar(200) DEFAULT NULL,
+  `RegistrationDate` timestamp NULL DEFAULT current_timestamp(),
+  `AssignedtoEmpId` varchar(150) DEFAULT NULL,
+  `AssigntoName` varchar(180) DEFAULT NULL,
+  `AssignedTime` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbltestrecord`
+--
+
+create table status_name(
+    id int not null,
+    desription varchar(50) not null,
+    primary key(id));
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpatients`
+--
+ALTER TABLE `tblpatients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblphlebotomist`
+--
+ALTER TABLE `tblphlebotomist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblreporttracking`
+--
+ALTER TABLE `tblreporttracking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbltestrecord`
+--
+ALTER TABLE `tbltestrecord`
+  ADD PRIMARY KEY (`id`);
+ 
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblpatients`
+--
+ALTER TABLE `tblpatients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tblphlebotomist`
+--
+ALTER TABLE `tblphlebotomist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblreporttracking`
+--
+ALTER TABLE `tblreporttracking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18,
+   add foreign key(Status) references status_name(id);
+
+--
+-- AUTO_INCREMENT for table `tbltestrecord`
+--
+ALTER TABLE `tbltestrecord`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8,
+  add foreign key(ReportStatus) references status_name(id);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+  <style>
+    .photo{
+    background-image: url('img/covid-19.jpg') ;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    /* background-position: center; */
+   
+}
+  </style>
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top" id="mainNav">
+    <div class="container">
+      <!-- <a class="navbar-brand js-scroll-trigger" href="#page-top">Home</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button> -->
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a  class="nav-link js-scroll-trigger" href="#about" >About Coronavirus </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Prevention</a>
+          </li>
+            <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="photo text-white">
+    <div class="container text-center">
+      <h1>Testing for coronavirus</h1>
+      <p class="lead"> <b>(COVID-19)</b></p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>About Coronavirus</h2>
+          <p class="lead">Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.
+
+Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age. 
+
+The best way to prevent and slow down transmission is to be well informed about the disease and how the virus spreads. Protect yourself and others from infection by staying at least 1 metre apart from others, wearing a properly fitted mask, and washing your hands or using an alcohol-based rub frequently. Get vaccinated when it’s your turn and follow local guidance.
+
+The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols. It is important to practice respiratory etiquette, for example by coughing into a flexed elbow, and to stay home and self-isolate until you recover if you feel unwell.</p>
+          <p class="lead">The COVID-19 virus spread primarily through droplet of saliva or discharge from the nose when an infected person coughs or sneezes so it’s important that you also practice respiratory etiquette.</p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Covid-19 Symptoms</h2>
+          <hr />
+<p><strong>Hight Fever 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+             <hr />
+<p><strong>Dry Cough 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+          <hr />
+<p><strong>Shortness of breath!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+            <li>Wash your Hands often</li>
+            <li>Wear A Face mask</li>
+            <li>Avoid contact with sick people</li>
+            <li>Always cover your cough or sneeze</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; My Website 2023</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
